1b34c7af14a3824b153272c866842c9b41de5a2c
farshid-shokrollahi <f.shokrollahi@hotmail.com>
2017-07-04 01:33:28 +0430

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Farshid Shokrollahi on ۱۳۹۶/۴/۱۳ ه‍.ش..
+//  Copyright © ۱۳۹۶ ه‍.ش. Artinweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
